01248d04a2fcfb95778ac026f163ae61c1fd1600
Racharla <w0514883@campus.nscc.ca>
2025-03-01 15:38:19 -0400

    Donw with Navbar basic setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,41 @@ const Navbar = () => {
   return (
     <nav className='bg-base-200'>
         <div className="navbar align-element">
-            <div className="navbar-start"></div>
-            <div className="navbar-center"></div>
-            <div className="navbar-end"></div>
+            <div className="navbar-start">
+                {/* Title */}
+                <NavLink to='/' className='hidden lg:flex btn btn-primary text-3xl items-center '>
+                L
+                </NavLink>
 
+                {/*Dropdown*/}
+                <div className="dropdown">
+                    <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+                        <FaBarsStaggered className='h-6 w-6'/>
+
+                    </label>
+                    <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200'>
+                        nav links
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal">Nav Links</ul>
+            </div>
+            <div className="navbar-end">
+                {/* Theme setup*/}
+                {/*  Cart Link */}
+
+                <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
+                <div className="indicator">
+                    <BsCart3 className='h-6 w-6'/>
+                    <span className='badge badge-sm badge-primary indicator-item'>
+                        8
+                    </span>
+                </div>
+                </NavLink>       
+
+            </div>
         </div>
-
     </nav>
   )
 }
