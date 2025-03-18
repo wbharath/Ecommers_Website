809c5e4b8343506868889445e98f2cbea100f0e7
Racharla <wbharath51@gmail.com>
2025-03-18 13:55:34 -0300

    Fixed the price in featured products

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from "react-router-dom"
+import { formatPrice } from "../utils"
 
 const ProductsGrid = () => {
     const {products} = useLoaderData()
@@ -8,6 +9,7 @@ const ProductsGrid = () => {
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {products.map((product)=>{
             const {title, price, image} = product.attributes
+            const dollarsAmount = formatPrice(price)
             return (
                 <Link to={`/products/${product.id}`} className="card w-full shadow-xl hover:shadow-2xl transition duration-300">
                     <figure className="px-4 pt-4">
@@ -15,7 +17,7 @@ const ProductsGrid = () => {
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title capitalize tracking-wider">{title}</h2>
-                        <span className="text-secondary">{price}</span>
+                        <span className="text-secondary">{dollarsAmount}</span>
                     </div>
                 </Link>
             )
